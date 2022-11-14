135b12503ed96a8f1d84d1da68d2bfbb22710d97
Reccote <uu78uu7841@gmail.com>
2022-11-14 16:57:09 +0900

    test

---
+++ Prefabs_UI_Test/Scripts/Scripts/TriggersPhone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggersPhone : MonoBehaviour {
+	public bool SignalTrigger;
+	public bool BatteryTrigger;
+	public bool SMSTrigger;
+
+	public int NumSignal, NumBattery;
+	public string TextSMS;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
