09b86d94f113b83d0b85eba417b5b6d308228777
YangUSeok <whaqlwnl0@naver.com>
2022-11-29 22:40:14 +0900

    XR ToolKit 중간쯤

--- First_Portfolio_ThemePark/Assets/02_Scripts/Player/USeok/ActivateTeleportationRay.cs
+++ First_Portfolio_ThemePark/Assets/02_Scripts/Player/USeok/ActivateTeleportationRay.cs
@@ -11,9 +11,12 @@ public class ActivateTeleportationRay : MonoBehaviour
     [SerializeField] InputActionProperty leftActivate;
     [SerializeField] InputActionProperty rightActivate;
 
+    [SerializeField] InputActionProperty leftCancle;
+    [SerializeField] InputActionProperty rightCancle;
+
     private void Update()
     {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        leftTeleportation.SetActive(leftCancle.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightCancle.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
