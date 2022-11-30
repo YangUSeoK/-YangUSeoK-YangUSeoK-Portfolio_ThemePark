4361896c573987ebbadf707321228de799ac2d30
Reccote <uu78uu7841@gmail.com>
2022-11-30 11:06:11 +0900

    소리UI(체크못함), 화면밝기조절 변경한 것(UIWorkingScene에서 작업함)

---
+++ First_Portfolio_ThemePark/Assets/02_Scripts/UI/ScreenLightCtrl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenLightCtrl : MonoBehaviour
+{
+    public Slider slider;
+    public Light ScreenLight;
+
+    private void Update()
+    {
+        ScreenLight.intensity = slider.value;
+    }
+
+}
