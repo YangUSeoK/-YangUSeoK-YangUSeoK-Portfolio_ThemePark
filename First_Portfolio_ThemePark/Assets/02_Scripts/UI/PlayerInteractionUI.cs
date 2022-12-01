625328235bfa8c19b0adf91496c5a8cb069c67e8
Reccote <uu78uu7841@gmail.com>
2022-12-01 18:52:59 +0900

    11

--- First_Portfolio_ThemePark/Assets/02_Scripts/UI/PlayerInteractionUI.cs
+++ First_Portfolio_ThemePark/Assets/02_Scripts/UI/PlayerInteractionUI.cs
@@ -5,34 +5,15 @@ using UnityEngine.UI;
 
 public class PlayerInteractionUI : MonoBehaviour
 {//22 11 29 김준우
-    public Image mItemPos;
-    private Item[] mGrabbableItem;
-    public Canvas mCanvas;
-    RectTransform mRectCanvas;
-    Camera mUICamera;
+    Image mItemPos;
+
     void Start()
     {
-        mItemPos.enabled = false;
-        mGrabbableItem = FindObjectsOfType<Item>();
-        mUICamera = mCanvas.worldCamera;
-        mRectCanvas = mCanvas.GetComponent<RectTransform>();
+
     }
 
     void Update()
     {
-        for(int mCnt=0; mCnt<mGrabbableItem.Length; mCnt++)//221130 김준우
-        {
-            if(mGrabbableItem[mCnt].GetComponent<Item>().mbUICatch==false)
-            {   
-                mItemPos.GetComponent<Image>().enabled = true;//이거 필요한가?
-                mGrabbableItem[mCnt].GetComponent<Item>().mbUICatch = true;
-                Instantiate(mItemPos,mCanvas.transform);
-            }
-            //if(mGrabbableItem[mCnt].GetComponent<Item>().mbIsHanded == true ||
-            //    mGrabbableItem[mCnt].GetComponent<Item>().mbIsInPocket == true)//쥐고 있거나 인벤토리에 있을경우
-            //{
-            //    this.mItemPos.enabled= false;
-            //}
-        }
+        
     }
 }
