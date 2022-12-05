23930e132eb968c9e599cde4555c80f53cc83aec
Reccote <uu78uu7841@gmail.com>
2022-12-05 14:42:49 +0900

    튜토리얼 ui백업

--- First_Portfolio_ThemePark/Assets/02_Scripts/UI/OnClickButton.cs
+++ First_Portfolio_ThemePark/Assets/02_Scripts/UI/OnClickButton.cs
@@ -9,7 +9,11 @@ public class OnClickButton : MonoBehaviour
     public GameObject mOptionBtn;
     public GameObject mGameOver;
     public GameObject mGameClear;
-    public GameObject mTutorialUI;
+
+    [Header("튜토리얼(물음표) UI")]
+    public GameObject[] mTutorialUI;
+    private int mTutorialCnt = 0;
+
     public void StartGame()
     {
         mMenu.SetActive(false); 
@@ -39,5 +43,44 @@ public class OnClickButton : MonoBehaviour
     public void QuitMenu()
     {
         mMenu.SetActive(false);
+    }
+    public void GoTutorial()
+    {
+        mMenu.SetActive(false);
+        mTutorialUI[0].SetActive(true);
+    }
+    public void TutorialGotoMenu()
+    {
+        for (int i = 0; i < mTutorialUI.Length; i++)
+        {
+            mTutorialUI[i].SetActive(false);
+        }
+        mMenu.SetActive(true);
+    }
+    public void TutorialGoRight()
+    {
+        if(mTutorialCnt<mTutorialUI.Length)
+        {
+            mTutorialUI[mTutorialCnt].SetActive(false);
+            ++mTutorialCnt;
+            mTutorialUI[mTutorialCnt].SetActive(true);
+        }
+        else if(mTutorialCnt>=mTutorialUI.Length)
+        {
+            mTutorialCnt = 0;
+        }
+    }
+    public void TutorialGoLeft()
+    {
+        if (mTutorialCnt < mTutorialUI.Length)
+        {
+            mTutorialUI[mTutorialCnt].SetActive(false);
+            --mTutorialCnt;
+            mTutorialUI[mTutorialCnt].SetActive(true);
+        }
+        else if (mTutorialCnt <= mTutorialUI.Length)
+        {
+            mTutorialCnt = mTutorialUI.Length;
+        }
     }
 }
