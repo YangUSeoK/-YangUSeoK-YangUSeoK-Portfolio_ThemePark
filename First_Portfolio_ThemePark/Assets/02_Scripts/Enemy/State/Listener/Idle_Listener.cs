ec7bdc73e75c7dd922d3dd7b5763c8beaabddc9b
ELTOP\Tir <hdin6@naver.com>
2022-12-06 12:25:53 +0900

    리스너좀비 수정

--- First_Portfolio_ThemePark/Assets/02_Scripts/Enemy/State/Listener/Idle_Listener.cs
+++ First_Portfolio_ThemePark/Assets/02_Scripts/Enemy/State/Listener/Idle_Listener.cs
@@ -11,16 +11,16 @@ public class Idle_Listener : EnemyState
     {
         Debug.Log("Idle ¿‘¿Â!");
         m_Enemy.Agent.speed = 0f;
-        //m_Enemy.Anim.SetTrigger("IsIdle");
+        m_Enemy.Anim.SetBool("IsIdle", true);
         m_Enemy.Audio[0].Play();
+        (m_Enemy as Enemy_Listener).CurVolumeLv = 0f;
     }
 
     public override void ExitState()
     {
         Debug.Log("Idle ≈¿Â!");
+        m_Enemy.Anim.SetBool("IsIdle", false);
         m_Enemy.Audio[0].Stop();
-        m_Enemy.Anim.SetTrigger("IsTraceSound");
-        m_Enemy.Audio[1].Play();
     }
 
     public override void Action()
